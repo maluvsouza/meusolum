439db381a503c13483a918c5252316e158e8effa
maluvsouza <maria.souza1260@etec.sp.gov.br>
2025-08-01 09:53:13 -0300

    Delete solum directory

--- solum/js/produto.js
+++
@@ -1,9 +0,0 @@
-function trocarImagem(imagemClicada) {
-    const imagemPrincipal = document.getElementById('imagemPrincipal');
-
-    // Troca os srcs (como se trocasse de lugar)
-    const tempSrc = imagemPrincipal.src;
-    imagemPrincipal.src = imagemClicada.src;
-    imagemClicada.src = tempSrc;
-  }
-  
--- solum/sobre-nos.php
+++
@@ -1,4 +0,0 @@
-<?php require_once("./utils/header.php") ?>
-<?php require_once("./utils/navbar.php") ?>
-
-<?php require_once("./utils/footer.php") ?>
